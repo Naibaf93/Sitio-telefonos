d00edba986f5cdd1954071110e914b77075cc2a0
Fabián Huertas <miloh9318@hotmail.com>
2025-08-16 20:05:40 -0500

    funcion consultarUno

--- js/app.js
+++ js/app.js
@@ -29,3 +29,27 @@ async function obtenerTodos() {
         console.error(error)
     }
 }
+
+async function consultarUno() {
+    try {
+        let id = document.getElementById('txtConsulta').value;
+
+        if (id === '') {
+            alert('No ha ingresado ningún ID');
+            return;
+        }
+
+        axios.get('https://my-json-server.typicode.com/fedegaray/telefonos/dispositivos/' + id)
+        .then(respuesta => {
+            let dispositivo = respuesta.data;
+            document.getElementById('consultaNombre').value = dispositivo.marca;
+            document.getElementById('consultaModelo').value = dispositivo.modelo;
+            document.getElementById('consultaColor').value = dispositivo.color;
+            document.getElementById('consultaAlmacenamiento').value = dispositivo.almacenamiento + ' GB';
+            document.getElementById('consultaProcesador').value = dispositivo.procesador;
+        })
+        .catch(error => { throw new Error("Error en la solicitud: " + error) })
+    } catch (error) {
+        console.error(error)
+    }
+}
